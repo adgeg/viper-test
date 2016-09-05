264a1ac83a07c6cde006b5543b2d26f26763d917
gadgeg <gadgeg@octo.com>
2016-09-05 08:39:49 +0200

    Test SimulateDelayLoginRepository

---
+++ repositories/src/test/java/fr/viper/repositories/login/SimulateDelayLoginRepositoryTest.java
@@ -0,0 +1,20 @@
+package fr.viper.repositories.login;
+
+import org.junit.Test;
+
+import fr.viper.core.login.LoginRepository;
+import fr.viper.core.login.LoginRequest;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class SimulateDelayLoginRepositoryTest {
+    @Test
+    public void getUser_ShouldTriggerMethodOnDecoratedRepository() throws LoginRepository.InvalidPasswordException, LoginRepository.UnknownUserException {
+        final LoginRequest loginRequest = new LoginRequest("id", "password");
+        final LoginRepository decorated = mock(LoginRepository.class);
+        final SimulateDelayLoginRepository repository = new SimulateDelayLoginRepository(decorated);
+        repository.getUser(loginRequest);
+        verify(decorated).getUser(loginRequest);
+    }
+}
